b1078c8e4f963211e6ef57cca72727392ea48dbb
Andrew Thomas <andr3w.l.thomas@gmail.com>
2016-03-10 12:21:58 -0500

    added i18n for models (and subsequently model forms)

--- translation/models.py
+++ translation/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
 class Article(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField()
+    title = models.CharField(_("Title"), max_length=255)
+    content = models.TextField(_("Content"))
