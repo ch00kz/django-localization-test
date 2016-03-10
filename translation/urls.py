cc4872df861d3bcc2fe8f8bf6e01fbb632355ad7
Andrew Thomas <andr3w.l.thomas@gmail.com>
2016-03-10 11:41:28 -0500

    Enabled django admin

--- translation/urls.py
+++ translation/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.i18n import i18n_patterns
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = i18n_patterns(
     '',
     url(r'^$', 'translation.views.home', name='home'),
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
